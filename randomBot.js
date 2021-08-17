c57ee725cfa036a1569bb98e9b24d5e0ebf358a3
Tim Leach <timleach635@gmail.com>
2021-08-17 11:53:06 +0100

    Make randomBot play legally

--- randomBot.js
+++ randomBot.js
@@ -1,6 +1,9 @@
 const moves = ["R", "P", "S", "D", "W"];
 const nMoves = moves.length;
 
+const nonDynamiteMoves = ["R", "P", "S", "W"];
+const nNonDynamiteMoves = nonDynamiteMoves.length;
+
 function randomMove() {
     // Generate a random integer between 0 and 4 (the length of the moves array)
     const randomInt = Math.floor(Math.random() * nMoves);
@@ -8,9 +11,32 @@ function randomMove() {
     return moves[randomInt];
 }
 
+function randomNonDynamiteMove() {
+    // Generate a random integer between 0 and 3 (the length of the nonDynamiteMoves array)
+    const randomInt = Math.floor(Math.random() * nNonDynamiteMoves);
+
+    return nonDynamiteMoves[randomInt];
+}
+
+function howManyDynamites(gamestate) {
+    let nDynamites = 0;
+
+    for (let i = 0; i < gamestate.rounds.length; i++) {
+        if (gamestate.rounds[i].p1 === "D") {
+            nDynamites++;
+        }
+    }
+
+    return nDynamites;
+}
+
 class Bot {
     makeMove(gamestate) {
-        return randomMove();
+        if (howManyDynamites(gamestate) < 100) {
+            return randomMove();
+        } else {
+            return randomNonDynamiteMove();
+        }
     }
 }
 
